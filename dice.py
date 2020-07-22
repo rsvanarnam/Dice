33cce588f1ba3dbef4b271963550b60a064c95ae
rsvanarnam <36892758+rsvanarnam@users.noreply.github.com>
2020-07-21 20:55:54 -0600

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,35 @@
+import random
+
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
+
+if dice1 == 1:
+    print("1")
+elif dice1 == 2:
+    print("2")
+elif dice1 == 3:
+    print("3")
+elif dice1 == 4:
+    print("4")
+elif dice1 == 5:
+    print("5")    
+elif dice1 == 6:
+    print("6")
+else:
+    print("Hm. Something isn't working.")
+
+if dice2 == 1:
+    print("1")
+elif dice2 == 2:
+    print("2")
+elif dice2 == 3:
+    print("3")
+elif dice2 == 4:
+    print("4")
+elif dice2 == 5:
+    print("5")    
+elif dice2 == 6:
+    print("6")
+else:
+    print("Hm. Something isn't working.")
+
